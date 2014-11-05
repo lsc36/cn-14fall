9f79aa0bc91533fe9ee3d5ace834da5f13b1528a
Shao-Chuan Lee <lsc36x@gmail.com>
2014-11-05 22:32:26 +0800

    read data from input, input parser

--- proj1/client.py
+++ proj1/client.py
@@ -1,19 +1,20 @@
 #!/usr/bin/env python3
 
 from ptt2 import PTT2
+from input_parser import InputParser
 
 
 def read_data():
-    data = dict()
-    from getpass import getpass
-    data['user'] = input("User: ")
-    data['passwd'] = getpass()
+    input_file = open('input.txt', 'r')
+    parser = InputParser()
+    data = parser.parse_all(input_file.read())
+    input_file.close()
     return data
 
 
 def main():
     data = read_data()
-    ptt2 = PTT2(data['user'], data['passwd'], True)
+    ptt2 = PTT2(data['login']['id'], data['login']['pass'], True)
 
 
 if __name__ == '__main__':
---
+++ proj1/input_parser.py
@@ -0,0 +1,28 @@
+from html.parser import HTMLParser
+
+
+class InputParser(HTMLParser):
+
+    def handle_starttag(self, tag, attrs):
+        if tag == 'id': self.idx1 = 'login'
+        elif tag == 'board': self.idx1 = 'post'
+        elif tag == 'w': self.idx1 = 'waterball'
+        elif tag == 'm': self.idx1 = 'mail'
+        
+        if tag == 'w' or tag == 'm': self.idx2 = 'id'
+        elif tag == 'p': self.idx2 = 'title'
+        else: self.idx2 = tag
+
+    def handle_endtag(self, tag):
+        self.idx2 = None
+
+    def handle_data(self, data):
+        if self.idx1 and self.idx2:
+            self.data[self.idx1][self.idx2] = data
+
+    def parse_all(self, s):
+        self.data = {idx1: dict()
+            for idx1 in ['login', 'post', 'waterball', 'mail']}
+        self.idx1 = self.idx2 = None
+        self.feed(s)
+        return self.data
