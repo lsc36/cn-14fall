e3e6fe49f50d1295fb4c59764ea00f856fd96610
Shao-Chuan Lee <lsc36x@gmail.com>
2014-11-07 00:35:01 +0800

    verbose message

--- proj1/client.py
+++ proj1/client.py
@@ -14,11 +14,18 @@ def read_data():
 
 def main():
     data = read_data()
+    print("Logging in as %s ..." % (data['login']['id']))
     ptt2 = PTT2(data['login']['id'], data['login']['pass'], True)
+    print("Posting article on board %s, title: %s ..."
+        % (data['post']['board'], data['post']['title']))
     ptt2.post(data['post']['board'], data['post']['title'],
         data['post']['content'])
+    print("Sending mail to %s, title: %s ..."
+        % (data['mail']['id'], data['mail']['title']))
     ptt2.mail(data['mail']['id'], data['mail']['title'],
         data['mail']['content'])
+    print("Sending waterball to %s, content: %s ..."
+        % (data['waterball']['id'], data['waterball']['content']))
     ptt2.waterball(data['waterball']['id'], data['waterball']['content'])
     ptt2.close()
 
